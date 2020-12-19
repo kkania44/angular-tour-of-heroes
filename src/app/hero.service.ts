bb50454d5238b2906d41d03fc5a99d6066179241
kkania44 <kaniakrzysztof99@gmail.com>
2020-12-19 18:43:49 +0100

    implement delete hero method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -63,6 +63,16 @@ export class HeroService {
     );
   }
 
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.httpClient.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id = ${id}`)),
+      catchError(this.handleError<Hero>('delete hero'))
+      );
+  }
+
 }
 
 
